cf02bc2f6592aa65a0d4d70cdb9fb0564d4a5a71
wari-1 <405770221@qq.com>
2019-07-15 15:56:18 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      stylus: {
-        'resolve url': true,
-        'import': []
-      }
-    }
-  },
-  pluginOptions: {
-    'cube-ui': {
-      postCompile: true,
-      theme: false
-    }
-  }
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-shequ/" : "/"
+};
